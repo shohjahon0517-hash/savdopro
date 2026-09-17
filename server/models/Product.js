4c201f80f8ba4456c285482edfa19f43b851e626
shohjahon0517-hash <shohjahon0517@gmail.com>
2026-09-17 13:17:03 +0500

    Add Product model

---
+++ server/models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  cost: {
+    type: Number,
+    default: 0,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  barcode: {
+    type: String,
+    unique: true,
+  },
+  qrCode: String,
+  category: String,
+  image: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
